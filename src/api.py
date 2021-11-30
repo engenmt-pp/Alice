722b09ad9dd1d436c7d298434b31096467553010
Michael Engen <mengen@paypal.com>
2021-11-30 11:36:26 -0500

    Renamed vars in my_secrets, clarified a docstring, added catch to store.recent_orders.

--- src/api.py
+++ src/api.py
@@ -12,7 +12,9 @@ from .my_secrets import (
     PARTNER_CLIENT_ID,
     PARTNER_ID,
     PARTNER_SECRET,
-    merchant_id,
+    MERCHANT_ID,
+    MERCHANT_SECRET,
+    MERCHANT_CLIENT_ID,
 )
 
 from datetime import datetime, timedelta, timezone
@@ -74,6 +76,25 @@ def request_access_token(client_id, secret):
 
     response = requests.post(endpoint, headers=headers, data=data, auth=(client_id, secret))
     response_dict = response.json()
+    try:
+        return response_dict["access_token"]
+    except KeyError as exc:
+        print(f"Encountered a KeyError: {exc}")
+        print(f"response_dict = {json.dumps(response_dict, indent=2)}")
+        raise exc
+
+
+def build_auth_assertion(client_id, merchant_payer_id):
+    """Build and return the PayPal Auth Assertion.
+
+    See https://developer.paypal.com/docs/api/reference/api-requests/#paypal-auth-assertion for details.
+    """
+    header = {"alg": "none"}
+    payload = {"iss": client_id, "payer_id": merchant_payer_id}
+    signature = b""
+    header_b64 = base64.b64encode(json.dumps(header).encode("ascii"))
+    payload_b64 = base64.b64encode(json.dumps(payload).encode("ascii"))
+    return b".".join([header_b64, payload_b64, signature])
 
     try:
         return response_dict["access_token"]
@@ -794,19 +815,6 @@ def get_transactions():
     return response_dict
 
 
-def build_auth_assertion(client_id, merchant_payer_id):
-    """Build and return the PayPal Auth Assertion.
-
-    See https://developer.paypal.com/docs/api/reference/api-requests/#paypal-auth-assertion for details.
-    """
-    header = {"alg": "none"}
-    payload = {"iss": client_id, "payer_id": merchant_payer_id}
-    signature = b""
-    header_b64 = base64.b64encode(json.dumps(header).encode("ascii"))
-    payload_b64 = base64.b64encode(json.dumps(payload).encode("ascii"))
-    return b".".join([header_b64, payload_b64, signature])
-
-
 def refund_order(capture_id):
 
     endpoint = (
@@ -814,7 +822,7 @@ def refund_order(capture_id):
     )
 
     client_id = PARTNER_CLIENT_ID  # partner client ID
-    merchant_payer_id = merchant_id  # merchant merchant ID
+    merchant_payer_id = MERCHANT_ID  # merchant merchant ID
     headers = build_headers()
     headers["PayPal-Auth-Assertion"] = build_auth_assertion(
         client_id, merchant_payer_id
@@ -827,7 +835,7 @@ def refund_order(capture_id):
     return response_dict
 
 
-def get_transactions():
+def get_transactions(as_merchant=False):
     """Get the transactions from the preceding four weeks.
 
     Docs: https://developer.paypal.com/docs/api/transaction-search/v1/
@@ -835,7 +843,15 @@ def get_transactions():
     end_date = datetime.now(tz=timezone.utc)
     start_date = end_date - timedelta(days=28)
 
-    headers = build_headers()
+    if as_merchant:
+        # This doesn't work!
+        headers = build_headers(client_id=MERCHANT_CLIENT_ID, secret=MERCHANT_SECRET)
+    else:
+        headers = build_headers()
+
+    headers["PayPal-Auth-Assertion"] = build_auth_assertion(
+        client_id=PARTNER_CLIENT_ID, merchant_payer_id=MERCHANT_ID
+    )
 
     data = {
         "start_date": start_date.isoformat(timespec="seconds"),
