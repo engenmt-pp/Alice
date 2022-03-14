3a73d6ac32d3b1886a68e206c7cec46b7ec43bf4
engenmt-pp <93000221+engenmt-pp@users.noreply.github.com>
2022-03-14 10:07:20 -0400

    Reorg (#11)
    * Reorganized app structure. Run by calling ./app.py
    
    * Modified handling of defaults after config change.
    
    * Updated webhooks to match new environment variable situation.
    
    * Moved status_text declaration in partner.status; added testing=False line in app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     app = create_app()
 
-    testing = True
+    # testing = True
+    testing = False
     if testing:
         app.config.from_object(TestingConfig)  # Enables debug-level logging
     else:
