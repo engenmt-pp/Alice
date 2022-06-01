57aa7de866c00ec182126540553bb8cbeabe89d7
engenmt-pp <93000221+engenmt-pp@users.noreply.github.com>
2022-06-01 15:37:45 -0400

    Added auth-capture (#20)
    * Added auth-capture
    
    * Moved BN code inclusion in headers to a flag in build_headers.
    
    * Unified spacing.
    
    * Moved order_id info to API endpoint for order capture.

--- src/api.py
+++ src/api.py
@@ -27,8 +27,11 @@ def log_and_request(method, endpoint, **kwargs):
     )
 
     response = methods_dict[method](endpoint, **kwargs)
-    if not response.ok:
-        raise Exception(f"API response is not okay: {response.text}")
+    if response.ok:
+        print(f'Response: {json.dumps(response.json(),indent=2)}')
+    else:
+        response_dict = json.loads(response.text)
+        raise Exception(f"API response is not okay: {json.dumps(response_dict,indent=2)}")
 
     return response
 
@@ -40,26 +43,31 @@ def request_access_token(client_id, secret):
     """
     endpoint = build_endpoint("/v1/oauth2/token")
     headers = {"Content-Type": "application/json", "Accept-Language": "en_US"}
+
     data = {"grant_type": "client_credentials"}
 
-    response = log_and_request(
-        "POST", endpoint, headers=headers, data=data, auth=(client_id, secret)
-    )
+    response = requests.post(endpoint, headers=headers, data=data, auth=(client_id, secret))
     response_dict = response.json()
     return response_dict["access_token"]
 
 
-def build_headers(client_id=None, secret=None):
+def build_headers(client_id=None, secret=None, include_bn_code=False):
     """Build commonly used headers using a new PayPal access token."""
     if client_id is None:
         client_id = current_app.config["PARTNER_CLIENT_ID"]
     if secret is None:
         secret = current_app.config["PARTNER_SECRET"]
+    
     access_token = request_access_token(client_id, secret)
-    return {
+    headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {access_token}",
     }
+
+    if include_bn_code:
+        headers["PayPal-Partner-Attribution-Id"] = current_app.config["PARTNER_BN_CODE"]
+    
+    return headers
 
 
 def build_auth_assertion(client_id=None, merchant_id=None):
@@ -111,6 +119,7 @@ def create_partner_referral_v1(tracking_id, return_url):
     """
     endpoint = build_endpoint("/v1/customer/partner-referrals")
     headers = build_headers()
+
     data = {
         "customer_data": {
             "customer_type": "MERCHANT",
@@ -170,6 +179,7 @@ def create_partner_referral_v2(tracking_id, return_url):
     """
     endpoint = build_endpoint("/v2/customer/partner-referrals")
     headers = build_headers()
+
     data = {
         "tracking_id": tracking_id,
         "operations": [
@@ -256,17 +266,15 @@ def get_referral_status(partner_referral_id):
 
 
 @bp.route("/create-order", methods=("POST",))
-def create_order():
+def create_order(include_platform_fees = True):
     """Call the /v2/checkout/orders API to create an order.
 
-    Requires `bn_code`, `price`, and `payee_merchant_id` fields in the request body.
+    Requires `price` and `payee_id` fields in the request body.
 
     Docs: https://developer.paypal.com/docs/api/orders/v2/#orders_create
     """
     endpoint = build_endpoint("/v2/checkout/orders")
-
-    headers = build_headers()
-    headers["PayPal-Partner-Attribution-Id"] = request.json["bn_code"]
+    headers = build_headers(include_bn_code=True)
 
     data = {
         "intent": "CAPTURE",
@@ -274,17 +282,49 @@ def create_order():
             {
                 "custom_id": "Up to 127 characters can go here!",
                 "payee": {"merchant_id": request.json["payee_id"]},
-                "payment_instruction": {
-                    "disbursement_mode": "INSTANT",
-                    "platform_fees": [
-                        {
-                            "amount": {
-                                "currency_code": "USD", 
-                                "value": "1.00"
-                            }
-                        }
-                    ]
+                "amount": {
+                    "currency_code": "USD",
+                    "value": request.json["price"],
                 },
+            }
+        ],
+    }
+
+    if include_platform_fees:
+        data['purchase_units'][0]["payment_instruction"] = {
+            "disbursement_mode": "INSTANT",
+            'platform_fees': [
+                {
+                    "amount": {
+                        "currency_code": "USD", 
+                        "value": "1.00"
+                    }
+                }
+            ]
+        }
+
+    response = log_and_request("POST", endpoint, headers=headers, data=json.dumps(data))
+    response_dict = response.json()
+    return jsonify(response_dict)
+
+
+@bp.route("/create-order-auth", methods=("POST",))
+def create_order_auth():
+    """Call the /v2/checkout/orders API to create an order with intent=AUTHORIZE.
+
+    Requires `price` and `payee_id` fields in the request body.
+
+    Docs: https://developer.paypal.com/docs/api/orders/v2/#orders_create
+    """
+    endpoint = build_endpoint("/v2/checkout/orders")
+    headers = build_headers(include_bn_code = True)
+
+    data = {
+        "intent": "AUTHORIZE",
+        "purchase_units": [
+            {
+                "custom_id": "Up to 127 characters can go here!",
+                "payee": {"merchant_id": request.json["payee_id"]},
                 "amount": {
                     "currency_code": "USD",
                     "value": request.json["price"],
@@ -298,13 +338,68 @@ def create_order():
     return jsonify(response_dict)
 
 
+@bp.route("/auth-capture/<order_id>", methods=("POST",))
+def authorize_and_capture_order(order_id):
+    """Authorize and and then capture the order.
+    
+    Requires `orderId` in the request body.
+    """
+    response_dict = authorize_order(order_id)
+    auth_id = response_dict['purchase_units'][0]['payments']['authorizations'][0]['id']
+    return capture_authorization(auth_id)
+
+
+def authorize_order(order_id):
+    """Authorize the order using the /v2/checkout/orders API.
+    
+    Docs: https://developer.paypal.com/docs/api/orders/v2/#orders_authorize
+    """
+
+    endpoint = build_endpoint(f"/v2/checkout/orders/{order_id}/authorize")
+    headers = build_headers()
+
+    response = log_and_request("POST", endpoint, headers=headers)
+    response_dict = response.json()
+    return response_dict
+
+
+def capture_authorization(auth_id, partner_fees = True):
+    """Capture the authorization with the given `auth_id` using the /v2/payments/ API.
+
+    Docs: https://developer.paypal.com/docs/api/payments/v2/#authorizations_capture
+    """
+    endpoint = build_endpoint(f"/v2/payments/authorizations/{auth_id}/capture")
+    headers = build_headers()
+
+    if partner_fees:
+        data = {
+            "payment_instruction": {
+                "disbursement_mode": "INSTANT",
+                "platform_fees": [
+                    {
+                        "amount": {
+                            "currency_code": "USD", 
+                            "value": "1.00"
+                        }
+                    }
+                ]
+            }
+        }
+    else:
+        data = {}
+
+    response = log_and_request("POST", endpoint, headers=headers, data=json.dumps(data))
+    response_dict = response.json()
+    return jsonify(response_dict)
+
 @bp.route("/capture-order", methods=("POST",))
 def capture_order():
     """Call the /v2/checkout/orders API to capture an order.
 
     Docs: https://developer.paypal.com/docs/api/orders/v2/#orders_capture
     """
-    endpoint = build_endpoint(f"/v2/checkout/orders/{request.json['orderId']}/capture")
+    order_id = request.json['orderId']
+    endpoint = build_endpoint(f"/v2/checkout/orders/{order_id}/capture")
     headers = build_headers()
 
     response = log_and_request("POST", endpoint, headers=headers)
