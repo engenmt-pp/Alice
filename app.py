75ae56f6fa85ec17bd76c1ee49002d4169f71188
Michael Engen <mengen@paypal.com>
2023-03-08 10:28:59 -0500

    MVP for onboarding

--- app.py
+++ app.py
@@ -9,8 +9,5 @@ app = create_app()
 app.config.from_object(PartnerOneConfig)
 app.config.from_object(MerchantOneConfig)
 
-# app.logger.info(f"Partner ID: {app.config['PARTNER_ID']}")
-# app.logger.info(f"Merchant ID: {app.config['MERCHANT_ID']}")
-
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
