07cbbad829c517da4fccb9f8ad51e816a108db05
Michael Engen <mengen@paypal.com>
2023-03-09 14:49:07 -0500

    Corrected app config

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # from config import PartnerOneConfig, MerchantOneConfig
-from config import MerchantOneConfig
+from config import PartnerOneConfig
+
+# from config import MerchantOneConfig
 from src import create_app
 
 
@@ -9,10 +11,8 @@ if __name__ == "__main__":
 
     app = create_app()
 
-    from partner_specific_config import PartnerInderConfig as PartnerOneConfig
-
     app.config.from_object(PartnerOneConfig)
-    app.config.from_object(MerchantOneConfig)
+    # app.config.from_object(MerchantOneConfig)
 
     # from partner_specific_config import PartnerMerchantConfig
 
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     app.config.from_object(MerchantAppConfig2)
 
     app.logger.info(f"Partner ID: {app.config['PARTNER_ID']}")
-    app.logger.info(f"Merchant ID: {app.config['MERCHANT_ID']}")
+    # app.logger.info(f"Merchant ID: {app.config['MERCHANT_ID']}")
 
     app.run(host="127.0.0.1", port=5000)
