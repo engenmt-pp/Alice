ecad0a9f04d4cc1092394da2909abf91b107cb49
Michael Engen <mengen@paypal.com>
2023-04-13 10:43:26 -0400

    Fixed landing page, expanded branded checkout nav

--- src/__init__.py
+++ src/__init__.py
@@ -27,7 +27,7 @@ def create_app():
     app.register_blueprint(store.bp)
     app.register_blueprint(store_form.bp)
     app.register_blueprint(partner_form.bp)
-    app.add_url_rule("/", endpoint="partner_form.onboarding")
+    app.add_url_rule("/", endpoint="store_form.checkout_branded")
 
     app.register_blueprint(store_merchant.bp)
 
