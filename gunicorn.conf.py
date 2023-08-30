3b8b4390a538f49f24628de6b3639c9907d5bef9
Michael Engen <mengen@paypal.com>
2023-08-30 15:09:22 -0400

    Updated formatting of access logs, added dynamic worker number assignment

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,12 @@
+from multiprocessing import cpu_count
+
 proc_name = "alice"
-workers = 1
+workers = 2 * cpu_count() + 1
 bind = "0.0.0.0:8000"
 wsgi_app = "app:app"
 # daemon = True
 accesslog = "access_log.log"
-access_log_format = (
-    '%(t)s %(h)s "%(f)s" "%(m)s %(U)s" %(s)s "%(a)s" "%({wsgi.input}e)s"'
-)
+access_log_format = '{"date_time": "%(t)s", "remote_address": "%(h)s", "referer": "%(f)s", "method": "%(m)s", "url_path": "%(U)s", "status": "%(s)s", "user_agent": "%(a)s", "request_time_in_seconds": "%(L)s"}'
 # Docs: https://docs.gunicorn.org/en/stable/settings.html#access-log-format
 # t -> date of the request (e.g., [30/Aug/2023:13:44:38 -0400])
 # h -> remote address (e.g., 127.0.0.1)
@@ -15,3 +15,4 @@ access_log_format = (
 # U -> URL path without query string (e.g., /API/orders/)
 # s -> status (e.g., 200)
 # a -> user agent (e.g., Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0)
+# L -> request time in decimal seconds
