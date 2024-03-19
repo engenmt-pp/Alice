55684bdc1ee1a4393c438ddc02b4375a3f74164e
engenmt-pp <93000221+engenmt-pp@users.noreply.github.com>
2024-03-19 11:07:07 -0400

    Css update (#173)
    * removed unused blocks, upgraded to css nesting
    
    * Added endblock labels
    
    * continued nesting
    
    * Finished first nesting pass
    
    * Corrected deflections
    
    * Converted tabs to new tabs, tab names are sometimes wrong
    
    * correct tab insertion order
    
    * Added sub-click always for tabs
    
    * Added billing address to HFv2 card transactions (#171)
    
    * Updated creaky hosted fields setups
    
    * Unified style files
    
    * Final css updates
    
    * typo

--- src/static/utils.js
+++ src/static/utils.js
@@ -54,81 +54,23 @@ function saveOptionsAndReloadPage() {
   location.reload()
 }
 
-function activate(element) {
-  element.classList.remove('inactive')
-  element.classList.add('active')
-}
-function deactivate(element) {
-  element.classList.remove('active')
-  element.classList.add('inactive')
-}
-
-function changeTopLevelNav() {
-  const nav = document.getElementById('top-level-nav')
-  const checkedInput = nav.querySelector('input:checked')
-  const checkedInputId = checkedInput.getAttribute('id')
-  const activeDivId = checkedInputId.replace('input', 'div')
-
-  nav.querySelectorAll('input').forEach((each) => {
-    const inputId = each.getAttribute('id').replace('input', 'div')
-    const divId = inputId.replace('input', 'div')
-    const div = document.getElementById(divId)
-    if (divId == activeDivId) {
-      activate(div)
-    } else {
-      deactivate(div)
-    }
-  })
-}
-
-
-function updateApiCalls() {
-  /* This event fires when an input in #div-api-calls > nav gets checked.
-   */
-  const apiCalls = document.getElementById('div-api-calls')
-  const checkedInput = apiCalls.querySelector('input:checked')
-  const checkedInputId = checkedInput.getAttribute('id')
-  const divId = checkedInputId.replace('input', 'div')
-  const divElement = document.getElementById(divId)
-
-  apiCalls.querySelectorAll('div').forEach(deactivate)
-  activate(divElement)
-}
-function createApiCallDiv(baseId, contents) {
+function createTabPanel(contents) {
   const div = document.createElement('div')
-
-  let n = 1
-  let divId = `div-api-call-${baseId}-${n}`
-  while (document.getElementById(divId)) {
-    n++
-    divId = `div-api-call-${baseId}-${n}`
-  }
-  div.setAttribute('id', divId)
+  div.setAttribute('role', 'tabpanel')
   div.innerText = contents
   div.classList.add('api-response')
-  return { div, n }
-}
-function createApiCallInput(baseId, n) {
-  const input = document.createElement('input')
-  input.setAttribute('type', 'radio')
-  input.setAttribute('name', 'api-calls-nav-tabs')
-  input.setAttribute('checked', true)
 
-  const inputId = `input-api-call-${baseId}-${n}`
-  input.setAttribute('id', inputId)
-  input.onchange = updateApiCalls
-
-  return input
+  return div
 }
-function createApiCallDownloadButton(baseId, curl) {
+function createDownloadButton(label, curl) {
   const button = document.createElement('button')
   button.innerHTML = 'Download for Postman'
   button.style.display = 'block'
-  button.classList.add('action')
   button.classList.add('postman')
+  button.setAttribute('data-method', '')
   button.setAttribute('type', 'button')
 
-  const fileName = `${baseId}.txt`
+  const fileName = `${label}.txt`
   button.onclick = function () {
     const elt = document.createElement('a')
     const href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(curl)
@@ -139,49 +81,42 @@ function createApiCallDownloadButton(baseId, curl) {
 
   return button
 }
-function createApiCallLabel(baseId, inputId, n) {
-  const label = document.createElement('label')
-  label.setAttribute('for', inputId)
-  let title = baseId
-  if (n > 1) {
-    title += ` (${n})`
-  }
-  label.innerText = title
+function createTab(label) {
+  const button = document.createElement('button')
+  button.setAttribute('role', 'tab')
+  button.innerText = label
 
-  return label
+  return button
 }
+
 function addApiCalls(formattedCalls, click = true) {
-  const apiCalls = document.getElementById('div-api-calls')
-  const apiCallsNav = apiCalls.querySelector('nav')
-  for (const baseId in formattedCalls) {
-    if (formattedCalls.hasOwnProperty(baseId)) {
-      // `baseId` is something like 'create-order'.
-      const { human, curl } = formattedCalls[baseId]
-      const { div, n } = createApiCallDiv(baseId, human)
-      const button = createApiCallDownloadButton(baseId, curl)
-      div.prepend(button)
-      apiCalls.append(div)
+  const theTabs = document.querySelector('the-tabs the-tabs')
+  const apiCallsTabList = document.querySelector('[role=tabpanel] [role=tablist]')
+  for (const label in formattedCalls) {
+    if (formattedCalls.hasOwnProperty(label)) {
+      // `label` is something like 'create-order'.
+      const { human, curl } = formattedCalls[label]
+      const tabPanel = createTabPanel(human)
+      const downloadButton = createDownloadButton(label, curl)
+      tabPanel.prepend(downloadButton)
+      theTabs.append(tabPanel) // `tabPanel` is the last child of `theTabs`
 
-      const input = createApiCallInput(baseId, n)
-      apiCallsNav.append(input)
-
-      const inputId = input.getAttribute('id')
-      const label = createApiCallLabel(baseId, inputId, n)
-      apiCallsNav.append(label)
-
-      if (click) {
-        const topLevelNavInput = document.getElementById('input-api-calls')
-        if (topLevelNavInput != null) {
-          topLevelNavInput.click()
-        }
-      }
-      updateApiCalls()
+      const tab = createTab(label)
+      apiCallsTabList.append(tab)
     }
+  }
+  theTabs.setupEvents()
+  const lastTab = apiCallsTabList.querySelector(':last-child')
+  if (lastTab) {
+    lastTab.click()
+  }
+  if (click) {
+    document.getElementById('tab-api-calls').click()
   }
 }
 
 function downloadAll() {
-  document.querySelectorAll('#div-api-calls button').forEach((button) => { button.click() })
+  document.querySelectorAll('[role=tabpanel] [role=tabpanel] button').forEach((button) => { button.click() })
 }
 
 function setupCredentials() {
@@ -222,9 +157,6 @@ export {
   setAuthHeader,
   getPartnerMerchantInfo,
   saveOptionsAndReloadPage,
-  activate,
-  deactivate,
-  changeTopLevelNav,
   addApiCalls,
   setupCredentials,
 }
